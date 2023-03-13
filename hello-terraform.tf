b69603485949f20e5cca3b0b3b1a33c38393e06c
Bartosz Pazdziur <bartosz.pa.pazdziur@partner.bmw.de>
2023-03-13 09:28:38 +0100

    update provider, format

--- hello-terraform.tf
+++ hello-terraform.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.70"
+      version = "~> 4.00"
     }
   }
 }
-
 
 provider "aws" {
   profile = var.profile
@@ -20,11 +19,12 @@ variable "profile" {
 variable "special_name" {
   description = "unique prefix for dynamo table"
 }
+
 resource "aws_dynamodb_table" "hello-table" {
   name         = "${var.special_name}-hello-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "UserId"
-  range_key = "GameTitle"
+  range_key    = "GameTitle"
 
   attribute {
     name = "UserId"
